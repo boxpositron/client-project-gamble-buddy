8c58a6e6beb207e5e1a65cbbf31413714dbb5d1c
David Ibia <david.ibia@boxmarshall.com>
2023-12-15 03:09:25 +0100

    GAM-9 Add AppHeader component

---
+++ components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <nav
+      class="flex min-h-full flex-row border-b-2 border-b-yellow-500 bg-slate-900 px-10 py-5 shadow-xl md:px-20"
+    >
+      <div class="left my-auto">
+        <nuxt-link
+          to="/"
+          class="text-xl font-bold text-yellow-500 no-underline"
+        >
+          Gamble Buddy
+        </nuxt-link>
+      </div>
+
+      <div class="right my-auto ml-auto">
+        <ul>
+          <li>
+            <nuxt-link class="text-md text-white no-underline" to="/blackjack">
+              BlackJack
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
