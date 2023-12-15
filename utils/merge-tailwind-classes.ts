828cec85bb858cf001cd7ac4af030940ddd29bda
David Ibia <david.ibia@boxmarshall.com>
2023-12-15 01:50:39 +0100

    GAM-2 Add merge-tailwind-classes utility function

---
+++ utils/merge-tailwind-classes.ts
@@ -0,0 +1,13 @@
+import { twMerge } from "tailwind-merge";
+
+export type ElementClass = { [key: string]: boolean };
+
+
+export default (elementclass: ElementClass): string => {
+  const activeClasses = Object.keys(elementclass)
+    .filter((key) => elementclass[key])
+    .map((key) => key);
+
+  return twMerge(activeClasses);
+
+}
