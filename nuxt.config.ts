79d5bf74b5c4eb986861789b6a748227f8df2408
David Ibia <david.ibia@boxmarshall.com>
2023-12-15 01:50:18 +0100

    GAM-2 Add Prettier configuration and update Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    "@nuxtjs/google-fonts",
+    "@pinia-plugin-persistedstate/nuxt",
+    [
+      "@pinia/nuxt",
+      {
+        disableVuex: true,
+        autoImports: ["acceptHMRUpdate"],
+      },
+    ],
+  ],
+  plugins: ["plugins/fontawesome.ts"],
+  pinia: {
+    storesDirs: ["stores"],
+  },
+  googleFonts: {
+    display: "swap",
+    preconnect: true,
+    families: {
+      Jost: true,
+    },
+  },
+  vite: {
+    server: {
+      hmr: {
+        protocol: "ws",
+        host: "0.0.0.0",
+      },
+    },
+  },
+  build: {
+    transpile: ["@fortawesome/vue-fontawesome"],
+  },
 })
