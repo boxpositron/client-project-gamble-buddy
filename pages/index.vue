4190bedc7f88792dddd91716f80cd8b251b1fa1d
David Ibia <david.ibia@boxmarshall.com>
2023-12-15 03:10:03 +0100

    GAM-9 Update index.vue with new page title and meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    
-  </div>
+  <div class="min-h-screen bg-teal-800"></div>
 </template>
 
 <script lang="ts" setup>
-
+useHead({
+  title: "Home | Gamble Buddy",
+  meta: [
+    {
+      name: "description",
+      content: "Home page.",
+    },
+  ],
+});
 </script>
 
-<style>
-
-</style>
+<style></style>
