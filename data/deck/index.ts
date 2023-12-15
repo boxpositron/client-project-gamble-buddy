e5c127b5257092c1fcd1315afde2e3b9fc044a19
David Ibia <david.ibia@boxmarshall.com>
2023-12-15 03:09:34 +0100

    GAM-9 Fix bug in login functionality

---
+++ data/deck/index.ts
@@ -0,0 +1,245 @@
+// White Cards
+import Clover10White from "~/assets/images/deck/white/Clovers_10_white.png"
+import Clover2White from "~/assets/images/deck/white/Clovers_2_white.png"
+import Clover3White from "~/assets/images/deck/white/Clovers_3_white.png"
+import Clover4White from "~/assets/images/deck/white/Clovers_4_white.png"
+import Clover5White from "~/assets/images/deck/white/Clovers_5_white.png"
+import Clover6White from "~/assets/images/deck/white/Clovers_6_white.png"
+import Clover7White from "~/assets/images/deck/white/Clovers_7_white.png"
+import Clover8White from "~/assets/images/deck/white/Clovers_8_white.png"
+import Clover9White from "~/assets/images/deck/white/Clovers_9_white.png"
+import CloverAWhite from "~/assets/images/deck/white/Clovers_A_white.png"
+import CloverJackWhite from "~/assets/images/deck/white/Clovers_Jack_white.png"
+import CloverKingWhite from "~/assets/images/deck/white/Clovers_King_white.png"
+import CloverQueenWhite from "~/assets/images/deck/white/Clovers_Queen_white.png"
+import Heart10White from "~/assets/images/deck/white/Hearts_10_white.png"
+import Heart2White from "~/assets/images/deck/white/Hearts_2_white.png"
+import Heart3White from "~/assets/images/deck/white/Hearts_3_white.png"
+import Heart4White from "~/assets/images/deck/white/Hearts_4_white.png"
+import Heart5White from "~/assets/images/deck/white/Hearts_5_white.png"
+import Heart7White from "~/assets/images/deck/white/Hearts_7_white.png"
+import Heart8White from "~/assets/images/deck/white/Hearts_8_white.png"
+import Heart9White from "~/assets/images/deck/white/Hearts_9_white.png"
+import HeartAWhite from "~/assets/images/deck/white/Hearts_A_white.png"
+import HeartJackWhite from "~/assets/images/deck/white/Hearts_Jack_white.png"
+import HeartKingWhite from "~/assets/images/deck/white/Hearts_King_white.png"
+import HeartQueenWhite from "~/assets/images/deck/white/Hearts_Queen_white.png"
+import Heart6White from "~/assets/images/deck/white/Heats_6_white.png"
+import Pike10White from "~/assets/images/deck/white/Pikes_10_white.png"
+import Pike2White from "~/assets/images/deck/white/Pikes_2_white.png"
+import Pike3White from "~/assets/images/deck/white/Pikes_3_white.png"
+import Pike4White from "~/assets/images/deck/white/Pikes_4_white.png"
+import Pike5White from "~/assets/images/deck/white/Pikes_5_white.png"
+import Pike6White from "~/assets/images/deck/white/Pikes_6_white.png"
+import Pike7White from "~/assets/images/deck/white/Pikes_7_white.png"
+import Pike8White from "~/assets/images/deck/white/Pikes_8_white.png"
+import Pike9White from "~/assets/images/deck/white/Pikes_9_white.png"
+import PikeAWhite from "~/assets/images/deck/white/Pikes_A_white.png"
+import PikeJackWhite from "~/assets/images/deck/white/Pikes_Jack_white.png"
+import PikeKingWhite from "~/assets/images/deck/white/Pikes_King_white.png"
+import PikeQueenWhite from "~/assets/images/deck/white/Pikes_Queen_white.png"
+import Tile10White from "~/assets/images/deck/white/Tiles_10_white.png"
+import Tile2White from "~/assets/images/deck/white/Tiles_2_white.png"
+import Tile3White from "~/assets/images/deck/white/Tiles_3_white.png"
+import Tile4White from "~/assets/images/deck/white/Tiles_4_white.png"
+import Tile5White from "~/assets/images/deck/white/Tiles_5_white.png"
+import Tile6White from "~/assets/images/deck/white/Tiles_6_white.png"
+import Tile7White from "~/assets/images/deck/white/Tiles_7_white.png"
+import Tile8White from "~/assets/images/deck/white/Tiles_8_white.png"
+import Tile9White from "~/assets/images/deck/white/Tiles_9_white.png"
+import TileAWhite from "~/assets/images/deck/white/Tiles_A_white.png"
+import TileJackWhite from "~/assets/images/deck/white/Tiles_Jack_white.png"
+import TileKingWhite from "~/assets/images/deck/white/Tiles_King_white.png"
+import TileQueenWhite from "~/assets/images/deck/white/Tiles_Queen_white.png"
+
+// Black Cards
+import Clover10Black from "~/assets/images/deck/black/Clovers_10_black.png"
+import Clover2Black from "~/assets/images/deck/black/Clovers_2_black.png"
+import Clover3Black from "~/assets/images/deck/black/Clovers_3_black.png"
+import Clover4Black from "~/assets/images/deck/black/Clovers_4_black.png"
+import Clover5Black from "~/assets/images/deck/black/Clovers_5_black.png"
+import Clover6Black from "~/assets/images/deck/black/Clovers_6_black.png"
+import Clover7Black from "~/assets/images/deck/black/Clovers_7_black.png"
+import Clover8Black from "~/assets/images/deck/black/Clovers_8_black.png"
+import Clover9Black from "~/assets/images/deck/black/Clovers_9_black.png"
+import CloverABlack from "~/assets/images/deck/black/Clovers_A_black.png"
+import CloverJackBlack from "~/assets/images/deck/black/Clovers_Jack_black.png"
+import CloverKingBlack from "~/assets/images/deck/black/Clovers_King_black.png"
+import CloverQueenBlack from "~/assets/images/deck/black/Clovers_Queen_black.png"
+import Heart10Black from "~/assets/images/deck/black/Hearts_10_black.png"
+import Heart2Black from "~/assets/images/deck/black/Hearts_2_black.png"
+import Heart3Black from "~/assets/images/deck/black/Hearts_3_black.png"
+import Heart4Black from "~/assets/images/deck/black/Hearts_4_black.png"
+import Heart5Black from "~/assets/images/deck/black/Hearts_5_black.png"
+import Heart7Black from "~/assets/images/deck/black/Hearts_7_black.png"
+import Heart8Black from "~/assets/images/deck/black/Hearts_8_black.png"
+import Heart9Black from "~/assets/images/deck/black/Hearts_9_black.png"
+import HeartABlack from "~/assets/images/deck/black/Hearts_A_black.png"
+import HeartJackBlack from "~/assets/images/deck/black/Hearts_Jack_black.png"
+import HeartKingBlack from "~/assets/images/deck/black/Hearts_King_black.png"
+import HeartQueenBlack from "~/assets/images/deck/black/Hearts_Queen_black.png"
+import Heart6Black from "~/assets/images/deck/black/Heats_6_black.png"
+import Pike10Black from "~/assets/images/deck/black/Pikes_10_black.png"
+import Pike2Black from "~/assets/images/deck/black/Pikes_2_black.png"
+import Pike3Black from "~/assets/images/deck/black/Pikes_3_black.png"
+import Pike4Black from "~/assets/images/deck/black/Pikes_4_black.png"
+import Pike5Black from "~/assets/images/deck/black/Pikes_5_black.png"
+import Pike6Black from "~/assets/images/deck/black/Pikes_6_black.png"
+import Pike7Black from "~/assets/images/deck/black/Pikes_7_black.png"
+import Pike8Black from "~/assets/images/deck/black/Pikes_8_black.png"
+import Pike9Black from "~/assets/images/deck/black/Pikes_9_black.png"
+import PikeABlack from "~/assets/images/deck/black/Pikes_A_black.png"
+import PikeJackBlack from "~/assets/images/deck/black/Pikes_Jack_black.png"
+import PikeKingBlack from "~/assets/images/deck/black/Pikes_King_black.png"
+import PikeQueenBlack from "~/assets/images/deck/black/Pikes_Queen_black.png"
+import Tile10Black from "~/assets/images/deck/black/Tiles_10_black.png"
+import Tile2Black from "~/assets/images/deck/black/Tiles_2_black.png"
+import Tile3Black from "~/assets/images/deck/black/Tiles_3_black.png"
+import Tile4Black from "~/assets/images/deck/black/Tiles_4_black.png"
+import Tile5Black from "~/assets/images/deck/black/Tiles_5_black.png"
+import Tile6Black from "~/assets/images/deck/black/Tiles_6_black.png"
+import Tile7Black from "~/assets/images/deck/black/Tiles_7_black.png"
+import Tile8Black from "~/assets/images/deck/black/Tiles_8_black.png"
+import Tile9Black from "~/assets/images/deck/black/Tiles_9_black.png"
+import TileABlack from "~/assets/images/deck/black/Tiles_A_black.png"
+import TileJackBlack from "~/assets/images/deck/black/Tiles_Jack_black.png"
+import TileKingBlack from "~/assets/images/deck/black/Tiles_King_black.png"
+import TileQueenBlack from "~/assets/images/deck/black/Tiles_Queen_black.png"
+
+
+export const SupportedDecks = {
+    white: "white",
+    black: "black"
+} as const
+
+
+const decks: Deck[] = [{
+    name: SupportedDecks.white,
+    cards: [
+        { name: "Clover 10", path: Clover10White },
+        { name: "Clover 2", path: Clover2White },
+        { name: "Clover 3", path: Clover3White },
+        { name: "Clover 4", path: Clover4White },
+        { name: "Clover 5", path: Clover5White },
+        { name: "Clover 6", path: Clover6White },
+        { name: "Clover 7", path: Clover7White },
+        { name: "Clover 8", path: Clover8White },
+        { name: "Clover 9", path: Clover9White },
+        { name: "Clover A", path: CloverAWhite },
+        { name: "Clover Jack", path: CloverJackWhite },
+        { name: "Clover King", path: CloverKingWhite },
+        { name: "Clover Queen", path: CloverQueenWhite },
+        { name: "Heart 10", path: Heart10White },
+        { name: "Heart 2", path: Heart2White },
+        { name: "Heart 3", path: Heart3White },
+        { name: "Heart 4", path: Heart4White },
+        { name: "Heart 5", path: Heart5White },
+        { name: "Heart 6", path: Heart6White },
+        { name: "Heart 7", path: Heart7White },
+        { name: "Heart 8", path: Heart8White },
+        { name: "Heart 9", path: Heart9White },
+        { name: "Heart A", path: HeartAWhite },
+        { name: "Heart Jack", path: HeartJackWhite },
+        { name: "Heart King", path: HeartKingWhite },
+        { name: "Heart Queen", path: HeartQueenWhite },
+        { name: "Pike 10", path: Pike10White },
+        { name: "Pike 2", path: Pike2White },
+        { name: "Pike 3", path: Pike3White },
+        { name: "Pike 4", path: Pike4White },
+        { name: "Pike 5", path: Pike5White },
+        { name: "Pike 6", path: Pike6White },
+        { name: "Pike 7", path: Pike7White },
+        { name: "Pike 8", path: Pike8White },
+        { name: "Pike 9", path: Pike9White },
+        { name: "Pike A", path: PikeAWhite },
+        { name: "Pike Jack", path: PikeJackWhite },
+        { name: "Pike King", path: PikeKingWhite },
+        { name: "Pike Queen", path: PikeQueenWhite },
+        { name: "Tile 10", path: Tile10White },
+        { name: "Tile 2", path: Tile2White },
+        { name: "Tile 3", path: Tile3White },
+        { name: "Tile 4", path: Tile4White },
+        { name: "Tile 5", path: Tile5White },
+        { name: "Tile 6", path: Tile6White },
+        { name: "Tile 7", path: Tile7White },
+        { name: "Tile 8", path: Tile8White },
+        { name: "Tile 9", path: Tile9White },
+        { name: "Tile A", path: TileAWhite },
+        { name: "Tile Jack", path: TileJackWhite },
+        { name: "Tile King", path: TileKingWhite },
+        { name: "Tile Queen", path: TileQueenWhite },
+    ]
+}, {
+    name: SupportedDecks.black,
+    cards: [
+        { name: "Clover 10", path: Clover10Black },
+        { name: "Clover 2", path: Clover2Black },
+        { name: "Clover 3", path: Clover3Black },
+        { name: "Clover 4", path: Clover4Black },
+        { name: "Clover 5", path: Clover5Black },
+        { name: "Clover 6", path: Clover6Black },
+        { name: "Clover 7", path: Clover7Black },
+        { name: "Clover 8", path: Clover8Black },
+        { name: "Clover 9", path: Clover9Black },
+        { name: "Clover A", path: CloverABlack },
+        { name: "Clover Jack", path: CloverJackBlack },
+        { name: "Clover King", path: CloverKingBlack },
+        { name: "Clover Queen", path: CloverQueenBlack },
+        { name: "Heart 10", path: Heart10Black },
+        { name: "Heart 2", path: Heart2Black },
+        { name: "Heart 3", path: Heart3Black },
+        { name: "Heart 4", path: Heart4Black },
+        { name: "Heart 5", path: Heart5Black },
+        { name: "Heart 6", path: Heart6Black },
+        { name: "Heart 7", path: Heart7Black },
+        { name: "Heart 8", path: Heart8Black },
+        { name: "Heart 9", path: Heart9Black },
+        { name: "Heart A", path: HeartABlack },
+        { name: "Heart Jack", path: HeartJackBlack },
+        { name: "Heart King", path: HeartKingBlack },
+        { name: "Heart Queen", path: HeartQueenBlack },
+        { name: "Pike 10", path: Pike10Black },
+        { name: "Pike 2", path: Pike2Black },
+        { name: "Pike 3", path: Pike3Black },
+        { name: "Pike 4", path: Pike4Black },
+        { name: "Pike 5", path: Pike5Black },
+        { name: "Pike 6", path: Pike6Black },
+        { name: "Pike 7", path: Pike7Black },
+        { name: "Pike 8", path: Pike8Black },
+        { name: "Pike 9", path: Pike9Black },
+        { name: "Pike A", path: PikeABlack },
+        { name: "Pike Jack", path: PikeJackBlack },
+        { name: "Pike King", path: PikeKingBlack },
+        { name: "Pike Queen", path: PikeQueenBlack },
+        { name: "Tile 10", path: Tile10Black },
+        { name: "Tile 2", path: Tile2Black },
+        { name: "Tile 3", path: Tile3Black },
+        { name: "Tile 4", path: Tile4Black },
+        { name: "Tile 5", path: Tile5Black },
+        { name: "Tile 6", path: Tile6Black },
+        { name: "Tile 7", path: Tile7Black },
+        { name: "Tile 8", path: Tile8Black },
+        { name: "Tile 9", path: Tile9Black },
+        { name: "Tile A", path: TileABlack },
+        { name: "Tile Jack", path: TileJackBlack },
+        { name: "Tile King", path: TileKingBlack },
+        { name: "Tile Queen", path: TileQueenBlack },
+    ]
+}]
+
+export type Deck = {
+    name: string
+    cards: Card[]
+}
+
+export type Card = {
+    name: string
+    path: string
+}
+
+
+export type SupportedDeck = typeof SupportedDecks[keyof typeof SupportedDecks]
+
+export function getDeck(deck: SupportedDeck): Deck {
+    return decks.find(d => d.name === deck)!
+}
