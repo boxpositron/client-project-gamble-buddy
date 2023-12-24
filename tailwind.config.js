30af8e2478ec06c22802146a9a5f55a3e1d4c61a
David Ibia <david.ibia@boxmarshall.com>
2023-12-24 22:35:31 +0100

    Add indigo colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          100: "#dae4df",
+          200: "#b6c9bf",
+          300: "#91aea0",
+          400: "#6d9380",
+          500: "#487860",
+          600: "#3a604d",
+          700: "#2b483a",
+          800: "#1d3026",
+          900: "#0e1813",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
