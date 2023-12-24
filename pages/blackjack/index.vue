118ccf1107e982e04394f2b9d22b79edf74d7e59
David Ibia <david.ibia@boxmarshall.com>
2023-12-24 22:34:48 +0100

    Add blackjack game page

---
+++ pages/blackjack/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="blackjack"></div>
+</template>
+
+<script lang="ts" setup>
+import { setupGame } from "~/pages/blackjack/game";
+
+useHead({
+  title: "Blackjack | Gamble Buddy",
+  meta: [
+    {
+      name: "description",
+      content: "Basic blackjack game.",
+    },
+  ],
+});
+
+definePageMeta({
+  layout: "game",
+});
+
+const blackjack = ref<HTMLDivElement | null>(null);
+
+onMounted(() => {
+  const { setupStage } = setupGame();
+  setupStage(blackjack);
+});
+</script>
+
+<style scoped></style>
