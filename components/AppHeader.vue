6422555a49285ee630c9f5902797badab20287f3
David Ibia <david.ibia@boxmarshall.com>
2024-02-12 02:58:38 +0100

    style(AppHeader.vue): remove unnecessary fixed class from nav element to improve readability and maintainability

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,8 +1,6 @@
 <template>
   <header>
-    <nav
-      class="fixed left-0 right-0 top-0 flex flex-row bg-indigo-500 px-10 py-5 shadow-md md:px-20"
-    >
+    <nav class="flex flex-row bg-indigo-500 px-10 py-5 shadow-md md:px-20">
       <div class="left my-auto">
         <nuxt-link to="/" class="text-xl font-bold text-white no-underline">
           Gamble Buddy
