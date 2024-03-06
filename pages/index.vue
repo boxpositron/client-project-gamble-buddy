54c446f4d0eb1f89f57930b2b1d218ed8cd09ae2
David Ibia <david.ibia@boxmarshall.com>
2024-03-06 01:25:35 +0100

    style(index.vue): reformat indentation and spacing for better readability and consistency

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="px-5 py-5 lg:px-20 lg:py-20">
-    <div class="flex w-full flex-col gap-2">
-      <nuxt-link
-        to="/blackjack"
-        class="relative text-center text-xl font-bold text-white transition-all duration-300 ease-in-out before:absolute before:left-0 before:top-1/2 before:h-1 before:w-full before:bg-white/10 hover:text-yellow-500"
-      >
-        Blackjack
-      </nuxt-link>
+    <div class="menu h-[calc(100vh-60px)] bg-[url('~/assets/images/background/welcome.jpg')] px-5 py-5 lg:px-20 lg:py-20">
+        <div class="flex w-full flex-col gap-2">
+            <nuxt-link to="/blackjack"
+                class="relative text-center text-xl font-bold text-white transition-all duration-300 ease-in-out before:absolute before:left-0 before:top-1/2 before:h-1 before:w-full before:bg-white/10 hover:text-yellow-500">
+                Blackjack
+            </nuxt-link>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
 useHead({
-  title: "Home | Gamble Buddy",
-  meta: [
-    {
-      name: "description",
-      content: "Home page.",
-    },
-  ],
+    title: "Home | Gamble Buddy",
+    meta: [
+        {
+            name: "description",
+            content: "Home page.",
+        },
+    ],
 });
 </script>
 
