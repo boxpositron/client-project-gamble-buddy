8b72f68b612a03711398393b47d64d9bd243da79
David Ibia <david.ibia@boxmarshall.com>
2024-03-06 01:22:02 +0100

    style(AppHeader.vue): improve header styling by adding gradient background color to enhance visual appeal

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
-  <header>
-    <nav class="flex flex-row bg-indigo-500 px-10 py-5 shadow-md md:px-20">
-      <div class="left my-auto">
-        <nuxt-link to="/" class="text-xl font-bold text-white no-underline">
-          Gamble Buddy
-        </nuxt-link>
-      </div>
-    </nav>
-  </header>
+    <header>
+        <nav class="flex flex-row bg-gradient-to-r from-indigo-900 to-indigo-500 px-10 py-5 shadow-md md:px-20">
+            <div class="left my-auto">
+                <nuxt-link to="/" class="text-xl font-bold text-white no-underline">
+                    Gamble Buddy
+                </nuxt-link>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <script lang="ts" setup></script>
